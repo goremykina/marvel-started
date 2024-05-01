a12dc9227734a22381516e8d599db0bc08b8a595
Alina Goremykina <a.goremykina98@gmail.com>
2024-05-01 17:13:16 +0200

    add prop-types

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,8 +1,10 @@
-import './charList.scss';
-import MarvelService from "../../services/MarvelService";
 import { Component } from "react";
+import PropTypes from "prop-types";
+import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
+import './charList.scss';
+
 
 class CharList extends Component {
     state = {
@@ -100,6 +102,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired,
 }
 
 export default CharList;
