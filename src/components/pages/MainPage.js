995af20b3769b724c56478eec377f8d8ed9bbba8
Alina Goremykina <a.goremykina98@gmail.com>
2024-05-30 16:29:49 +0200

    Add pages: MainPage.js and ComicsPage.js

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,26 @@
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import decoration from '../../resources/img/vision.png';
+import { useState } from "react";
+
+const MainPage = () => {
+    const [selectedCharId, setSelectedCharId] = useState(null)
+
+    const onCharSelected = (id) => {
+        setSelectedCharId(id);
+    }
+
+    return (
+        <>
+            <RandomChar/>
+            <div className="char__content">
+                <CharList selectedCharId={selectedCharId} onCharSelected={onCharSelected}/>
+                <CharInfo charId={selectedCharId}/>
+            </div>
+            <img className="bg-decoration" src={decoration} alt="vision"/>
+        </>
+    )
+}
+
+export default MainPage;
